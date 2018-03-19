4049f669141b057cef3ca7c8e13d473a4612fc63
Neveh Allon <nevehallon@gmail.com>
2018-03-19 17:41:42 +0200

    Added enimies and player sprites to game canvas

--- js/engine.js
+++ js/engine.js
@@ -13,12 +13,12 @@
  * writing app.js a little simpler to work with.
  */
 
-const Engine = (function(global) {
+var Engine = (function(global) {
   /* Predefine the variables we'll be using within this scope,
    * create the canvas element, grab the 2D context for that canvas
    * set the canvas elements height/width and add it to the DOM.
    */
-  const doc = global.document,
+  var doc = global.document,
     win = global.window,
     canvas = doc.createElement('canvas'),
     ctx = canvas.getContext('2d'),
@@ -173,7 +173,8 @@ const Engine = (function(global) {
     'images/water-block.png',
     'images/grass-block.png',
     'images/enemy-bug.png',
-    'images/char-boy.png'
+    'images/char-boy.png',
+    'images/char-horn-girl.png'
   ]);
   Resources.onReady(init);
 
